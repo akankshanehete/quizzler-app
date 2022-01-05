e6d009bf9baa345102d54344f40a62f535e0f8e2
akankshanehete <nehetea@mcmaster.ca>
2022-01-05 11:54:45 -0500

    created class based tkinter ui

--- ui.py
+++ ui.py
@@ -1,4 +1,14 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window['bg'] = THEME_COLOR
+        self.window.geometry("340x500")
+
+        self.window.mainloop()
 
 
